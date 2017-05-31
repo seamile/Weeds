aa6fde0aff359154a41fe05fca5eb85db2a0d15d
Seamile <lanhuermao@gmail.com>
2017-05-31 21:11:40 +0800

    添加身份证检查脚本

---
+++ Scripts/idcard.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+
+def get_check_code(id_code):
+    s = 0
+    weight = lambda i: (1 << (17 - i)) % 11
+    for i, a in enumerate(id_code[:-1]):
+        s += int(a) * weight(i)
+    n = (12 - s % 11) % 11
+
+    return 'X' if n == 10 else str(n)
+
+
+def convert_id_code(id_code):
+    if isinstance(id_code, (list, tuple)):
+        id_code = ''.join(i for i in id_code)
+    elif isinstance(id_code, bytes):
+        id_code = id_code.decode('utf8')
+    return id_code.upper()
+
+
+def check_id(id_code):
+    if not isinstance(id_code, (str, bytes, list, tuple)):  # type check
+        raise TypeError('Type of the id code is wrong')
+    elif len(id_code) != 18:                                # length check
+        raise ValueError('Length of the id code is invalid')
+    else:
+        id_code = convert_id_code(id_code)
+        if id_code[-1] not in '0123456789X':
+            raise ValueError('Check code is invalid')
+
+    check_code = get_check_code(id_code)
+    return check_code == id_code[-1]
